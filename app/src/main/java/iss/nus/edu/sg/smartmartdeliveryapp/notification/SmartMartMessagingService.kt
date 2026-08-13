8e7c5750bcc6a6f5dc59e557284381da2ad641df
pwtansg <pwtan.edu@gmail.com>
2026-08-13 10:17:16 +0800

    Add firebase job assigned push notification

--- app/src/main/java/iss/nus/edu/sg/smartmartdeliveryapp/notification/SmartMartMessagingService.kt
+++ app/src/main/java/iss/nus/edu/sg/smartmartdeliveryapp/notification/SmartMartMessagingService.kt
@@ -5,6 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
@@ -26,6 +27,10 @@ class SmartMartMessagingService : FirebaseMessagingService() {
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
+        Log.d(
+            "FCM_MESSAGE",
+            "Received: ${message.notification?.body}"
+        )
         super.onMessageReceived(message)
 
         val title =
