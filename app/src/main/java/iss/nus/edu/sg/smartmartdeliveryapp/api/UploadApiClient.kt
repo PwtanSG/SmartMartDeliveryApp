0626df5bd6c367dc2bd0172b7f1eaca31ad83cea
pwtansg <pwtan.edu@gmail.com>
2026-08-19 20:53:26 +0800

    Fix - upload

--- app/src/main/java/iss/nus/edu/sg/smartmartdeliveryapp/api/UploadApiClient.kt
+++ app/src/main/java/iss/nus/edu/sg/smartmartdeliveryapp/api/UploadApiClient.kt
@@ -1,20 +1,36 @@
 package iss.nus.edu.sg.smartmartdeliveryapp.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object UploadApiClient {
 
     private const val BASE_URL =
         "https://mwpd6bfwfd.execute-api.us-east-1.amazonaws.com/"
 
-    val uploadApi: UploadApiService by lazy {
+    private val loggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    private val httpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    val uploadApi: UploadApiService =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(httpClient)
             .addConverterFactory(
                 GsonConverterFactory.create()
             )
             .build()
             .create(UploadApiService::class.java)
-    }
 }
